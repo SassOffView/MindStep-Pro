3429604de3c8aa23a458d6030d3f37889896a9c9
SassOffView <eyesasa87@gmail.com>
2026-02-17 16:35:26 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='mindstep-v5.3';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./logo.png','./badge-icons.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
